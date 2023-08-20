5fc862e589dbb04ef5727c7fa8b465ae62c92c00
Tanya <krajnikova2004@gmail.com>
2023-08-20 23:04:14 +0100

    Create: entity model, base methods

---
+++ src/main/java/org/example/app/model/User.java
@@ -0,0 +1,57 @@
+package org.example.app.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("id")
+    private int id;
+    @SerializedName("first_name")
+    private String firstName;
+    @SerializedName("last_name")
+    private String lastName;
+    @SerializedName("email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
